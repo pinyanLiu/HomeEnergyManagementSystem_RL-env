a842cad21a134d3c208922bb50b739e1f01cb4e0
colinWang <colin861209@gmail.com>
2022-02-10 19:56:25 +0800

    writing step

--- lib/enviroment/enviroment.py
+++ lib/enviroment/enviroment.py
@@ -1,60 +1,75 @@
+import gym
 from  gym import Env
-from  gym.spaces import Discrete
+from  gym import spaces
 import numpy as np
-
-class CemsEnvClass(Env):
-    def __init__(self,info = 0) :
-        #Actions we take
-        # 0. grid+pv+bat->load; 1. grid+bat->load; 2.grid+pv->load; 3.grid->load; 4.grid+pv->bat+load; 5.grid->bat+load
+from random import randint
+class HemsEnv(Env):
+    def __init__(self,info) :
+        '''
+        Action space
+        observation space
+        '''
         self.info = info
-        self.action_space = Discrete(6)
-        self.all_day_long = 96
-        self.price = []
-        self.battery_capacity = 10000
-        self.SOC_threshold = 0.4
-        self.state = {'sample time':0,
-                    'grid price': self.info['grid price'][0],
-                    'PV': self.info['PV'][0],
-                    'SOC':0.2,
-                    
-                    'Pload':self.info['Pload'][0]}
-    
+        self.BaseParameter = info.experimentData['BaseParameter']
+        self.GridPrice = info.experimentData['GridPrice']
+        self.PV = info.experimentData['PV']
+        #pick one day from 360 days
+        i = randint(1,360)
+        self.Load = info.experimentData['Load'].iloc[:,i]
+        #action we take (charge , discharge , stay)
+        self.action_space = spaces.Discrete(3)
+        #observation space
+        upperLimit = np.array(
+            [
+                self.BaseParameter['BaseParameter'].loc[self.BaseParameter['BaseParameter']['parameter_name']=='SOCmax',['value']],
+                self.BaseParameter['BaseParameter'].loc[self.BaseParameter['BaseParameter']['parameter_name']=='PowerMaxCharge',['value']],
+            ],
+            dtype=np.float32,
+        )
+        lowerLimit = np.array(
+            [
+                self.BaseParameter['BaseParameter'].loc[self.BaseParameter['BaseParameter']['parameter_name']=='SOCmin',['value']],
+                self.BaseParameter['BaseParameter'].loc[self.BaseParameter['BaseParameter']['parameter_name']=='PowerMaxdisCharge',['value']],
+            ],
+            dtype=np.float32,
+        )
+        self.observationSpace = spaces.Box(lowerLimit,upperLimit,dtype=np.float32)
+        self.state = None
     def step(self,action):
+        '''
+        interaction of each state(changes while taking action)
+        Rewards
+        Episode Termination condition
+        '''
+        #error message if getting wrong action
+        err_msg = f"{action!r} ({type(action)}) invalid"
+        assert self.action_space.contains(action),err_msg
+
         #STATE
-        self.all_day_long -=1
-        self.state['sample time']+=1
-            #get grid price
-        self.state['grid price']=self.info['grid price'][self.state['sample time']]
-        self.state['PV'] = self.info['PV'][self.state['sample time']]
-        self.state['Pload'] = self.info['Pload'][self.state['sample time']]
-        #ACTION 
-            # 0. grid+pv+bat->load
-        if action == 0:
-            self.state['SOC']-=0.1
-            self.price.append((self.state['Pload']-0.1*self.battery_capacity-self.state['PV'])*self.state['grid price'])
+        sampleTime,load,pv,soc,price = self.state
+        sampleTime = sampleTime+1
+
+
+        #interaction
+        #prevent the agent still want to charge while the battery is full of electricity
+        if action == 0 and soc != 1:
+            # 0. charging
+            soc = soc+0.1
+            #calculate the cost at this sampletime (multiple 0.25 is for transforming price per hour into per min)
+            cost = price * 0.25 ( load + 0.1*self.BaseParameter['BaseParameter'].loc[self.BaseParameter['BaseParameter']['parameter_name']=='batteryCapacity',['value']] - pv  )
+
+        #prevent the agent still want to discharge while the battery is lack of electricity
+        elif action == 1 and soc != 0:
+            # 1. discharging
+            soc = soc-0.1
+            #calculate the cost at this sampletime (multiple 0.25 is for transforming price per hour into per min)
+            cost = price * 0.25 ( load - 0.1*self.BaseParameter['BaseParameter'].loc[self.BaseParameter['BaseParameter']['parameter_name']=='batteryCapacity',['value']] - pv  )
+
+            # 2.stay
+        else :
+            #calculate the cost at this sampletime (multiple 0.25 is for transforming price per hour into per min)
+            cost = price * 0.25 ( load - pv  )
             
-            # 1. grid+bat->load
-        elif action ==1:
-            self.state['SOC']-=0.1
-            self.price.append((self.state['Pload']-(0.1*self.battery_capacity))*self.state['grid price'])
-       
-            # 2.grid+pv->load
-        elif action ==2:
-            self.price.append((self.state['Pload']-self.state['PV'])*self.state['grid price'])
-        
-            # 3.grid->load
-        elif action ==3:
-            self.price.append(self.state['Pload']*self.state['grid price'])
-        
-            # 4.grid+pv->bat+load
-        elif action == 4:
-            self.state['SOC']+=0.1
-            self.price.append((self.state['Pload']+(0.1*self.battery_capacity)-self.state['PV'])*self.state['grid price'])
-        
-            # 5.grid->bat+load
-        elif action == 5:
-            self.state['SOC']+=0.1
-            self.price.append((self.state['Pload']+(0.1*self.battery_capacity))*self.state['grid price'])
 
 
             #check if all day is done
@@ -64,15 +79,21 @@ class CemsEnvClass(Env):
             done = False
 
 
+        done = bool(
+            sampleTime ==95
+        )
 
         #REWARD
-        if done ==True :
-            if self.state['SOC']<self.SOC_threshold:
-                reward = sum(self.state['Pload']*self.state['grid price'])-sum(self.price)
-            if self.state['SOC']>=self.SOC_threshold:
-                reward = 2*(sum(self.state['Pload']*self.state['grid price'])-sum(self.price))
-        else:
-            reward = 0
+        reward = []
+        if not done:
+            if soc == 1 and action == 1:
+                reward.append(-1)
+            elif soc == 0 and action == -1 :
+                reward.append(-1)
+
+
+
+
         #set placeholder for infomation
         info = {}
         return self.state,reward,done,info
@@ -81,11 +102,14 @@ class CemsEnvClass(Env):
     def render(self):
         pass
     def reset(self):
+        '''
+        Starting State
+        '''
         #reset state
         self.state = {'sample time':0,'grid price': self.info['grid price'][0],'PV': self.info['PV'][0],'SOC':0.2,'Pload':self.info['Pload'][0]}
         #reset time_block
         self.all_day_long = 96
-        return self.state
+        return np.array(self.state,dtype=np.float32)
 
 
 if __name__ == '__main__':
