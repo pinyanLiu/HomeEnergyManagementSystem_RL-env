ca26c595efa8ced180657634196ce57d105a4196
pinyanLiu <chig20503@gmail.com>
2022-03-30 17:21:15 +0800

    fix bug in env

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from tensorforce.execution import Runner  
 
 runner = Runner(
-    agent= 'agent/D3qn.json',
+    agent= 'agent/DDPG.json',
     environment=dict(environment='gym', level='Hems-v0'),
     max_episode_timesteps=96,
 )
