2d66bd623bccd9cfb4ddc0b37d660ec7e1208e5f
pinyanLiu <chig20503@gmail.com>
2022-04-21 21:29:02 +0800

    hp

--- train.py
+++ train.py
@@ -3,12 +3,13 @@ from tensorforce import Agent
 import sys
 
 if __name__ == '__main__':
-    if sys.argv[1] == 'pretrain':
-        runner = Runner(
-            environment=dict(environment='gym', level='Hems-v0'),
-            agent= Agent.load(directory = 'saver_dir',format='checkpoint'),
-            max_episode_timesteps=96,
-        )
+    if len(sys.argv) >2:    
+        if sys.argv[1] == 'pretrain':
+            runner = Runner(
+                environment=dict(environment='gym', level='Hems-v0'),
+                agent= Agent.load(directory = 'saver_dir',format='checkpoint'),
+                max_episode_timesteps=96,
+            )
     else:
         runner = Runner( 
             environment=dict(environment='gym', level='Hems-v0'),
@@ -18,5 +19,5 @@ if __name__ == '__main__':
 
 
 
-    runner.run(num_episodes=540,save_best_agent='bestAgent')
+    runner.run(num_episodes=1200,save_best_agent='bestAgent')
     runner.close()
