844f9e72a043417415cabfa5aba2baaa9bb66a7c
pinyanLiu <chig20503@gmail.com>
2022-04-22 17:26:54 +0800

    test SOC part

--- test.py
+++ test.py
@@ -2,181 +2,205 @@ from tensorforce import Agent,Environment
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
-def main():
-    environment=Environment.create(environment='gym',level='Hems-v1')
-    agent = Agent.load(directory = 'saver_dir',format='checkpoint',environment=environment)
+class Test():
+    def __init__(self,mode) :
+        self.mode = mode
 
-    soc = []
-    load = []
-    pv = []
-    totalReward = 0
-    monthlySoc = pd.DataFrame()
-    monthlyRemain = pd.DataFrame()
-    price = []
-    for i in range(12):
-        states = environment.reset()
-        internals = agent.initial_internals()
-        terminal = False
-        while not terminal:
-            actions, internals = agent.act(
-                states=states, internals=internals, independent=True, deterministic=True
-            )
-            states, terminal, reward = environment.execute(actions=actions)
-            load.append(states[1])
-            pv.append(states[2])
-            soc.append(states[3])
-            totalReward += reward
-            if i == 11:
-                price.append(states[4])
+    def main(self):
+    #run test result in different env
+        if self.mode == 'soc':
+            self.__testInSoc__()
+        
+        elif self.mode == 'load':
+            self.__testInLoad__()
+    #plot result
+        self.__plotResult__()
 
-        remain = [load[j]-pv[j] for j in range(len(load))]
-        price = [(price[i]-np.min(price))/(np.max(price)-np.min(price)) for i in range(len(price))] #normalize price to [0,1] 
-        monthlySoc.insert(i,column=str(i+1),value=soc)
-       # monthlyLoad.insert(i,column=str(i+1),value=load)
-       # monthlyPv.insert(i,column=str(i+1),value=pv)
-        monthlyRemain.insert(i,column=str(i+1),value=remain)
-        load.clear()
-        pv.clear()
-        soc.clear()
-    print('Agent average episode reward: ', totalReward/12 )
+    def __testInSoc__(self):
+        self.environment = dict(environment='gym', level='Hems-v1')
+        self.agent = Agent.load(directory = 'saver_dir',format='checkpoint',environment=environment)
+        soc = []
+        load = []
+        pv = []
+        totalReward = 0
+        self.monthlySoc = pd.DataFrame()
+        self.monthlyRemain = pd.DataFrame()
+        self.price = []
+        for month in range(12):
+            states = self.environment.reset()
+            internals = self.agent.initial_internals()
+            terminal = False
+            while not terminal:
+                actions, internals = self.agent.act(
+                    states=states, internals=internals, independent=True, deterministic=True
+                )
+                states, terminal, reward = self.environment.execute(actions=actions)
+                load.append(states[1])
+                pv.append(states[2])
+                soc.append(states[3])
+                totalReward += reward
+                if month == 11:
+                    price.append(states[4])
 
-#plot the result
+            remain = [load[sampletime]-pv[sampletime] for sampletime in range(96)]
+            #normalize price to [0,1]
+            price = [(price[month]-np.min(price))/(np.max(price)-np.min(price)) for month in range(len(price))]  
+            self.monthlySoc.insert(month,column=str(month+1),value=soc)
+            # monthlyLoad.insert(month,column=str(month+1),value=load)
+            # monthlyPv.insert(month,column=str(month+1),value=pv)
+            self.monthlyRemain.insert(month,column=str(month+1),value=remain)
+            load.clear()
+            pv.clear()
+            soc.clear()
+        print('Agent average episode reward: ', totalReward/12 )
 
-    plt.rcParams["figure.figsize"] = (12.8, 9.6)
-    fig,axes = plt.subplots(6,2)
-    plt.title("SOC and Price for each month")
-    #plt.xlabel("sampleTime")
-    ax1=axes[0,0]
-    ax2=axes[0,1]
-    ax3=axes[1,0]
-    ax4=axes[1,1]
-    ax5=axes[2,0]
-    ax6=axes[2,1]
-    ax7=axes[3,0]
-    ax8=axes[3,1]
-    ax9=axes[4,0]
-    ax10=axes[4,1]
-    ax11=axes[5,0]
-    ax12=axes[5,1]
-    sub1=ax1.twinx()
-    sub2=ax2.twinx()
-    sub3=ax3.twinx()
-    sub4=ax4.twinx()
-    sub5=ax5.twinx()
-    sub6=ax6.twinx()
-    sub7=ax7.twinx()
-    sub8=ax8.twinx()
-    sub9=ax9.twinx()
-    sub10=ax10.twinx()
-    sub11=ax11.twinx()
-    sub12=ax12.twinx()
+    def __testInLoad__(self):
+        pass
 
-    ax1.set_ylabel('SOC')
-    ax1.plot(range(len(monthlySoc['1'][:])), monthlySoc['1'][:], label = "Jan",color='red')    
-    ax1.plot(range(len(price)), price, label = "price")
-    ax1.set_title('Jan')
+    def __plotResult__(self):
+        plt.rcParams["figure.figsize"] = (12.8, 9.6)
+        fig,axes = plt.subplots(6,2)
+        plt.title("SOC and Price for each month")
+        #plt.xlabel("sampleTime")
+        ax1=axes[0,0]
+        ax2=axes[0,1]
+        ax3=axes[1,0]
+        ax4=axes[1,1]
+        ax5=axes[2,0]
+        ax6=axes[2,1]
+        ax7=axes[3,0]
+        ax8=axes[3,1]
+        ax9=axes[4,0]
+        ax10=axes[4,1]
+        ax11=axes[5,0]
+        ax12=axes[5,1]
+        sub1=ax1.twinx()
+        sub2=ax2.twinx()
+        sub3=ax3.twinx()
+        sub4=ax4.twinx()
+        sub5=ax5.twinx()
+        sub6=ax6.twinx()
+        sub7=ax7.twinx()
+        sub8=ax8.twinx()
+        sub9=ax9.twinx()
+        sub10=ax10.twinx()
+        sub11=ax11.twinx()
+        sub12=ax12.twinx()
+        
+        if self.mode == 'soc':
+            ax1.set_ylabel('SOC')
+            ax1.plot(range(len(self.monthlySoc['1'][:])), self.monthlySoc['1'][:], label = "Jan",color='red')    
+            ax1.plot(range(len(self.price)), self.price, label = "price")
+            ax1.set_title('Jan')
 
-    ax2.set_ylabel('SOC')
-    ax2.plot(range(len(monthlySoc['2'][:])), monthlySoc['2'][:], label = "Feb",color='red')
-    ax2.plot(range(len(price)), price, label = "price")
-    ax2.set_title('Feb')
+            ax2.set_ylabel('SOC')
+            ax2.plot(range(len(self.monthlySoc['2'][:])), self.monthlySoc['2'][:], label = "Feb",color='red')
+            ax2.plot(range(len(self.price)), self.price, label = "price")
+            ax2.set_title('Feb')
 
-    ax3.set_ylabel('SOC')
-    ax3.plot(range(len(price)), price, label = "price")
-    ax3.plot(range(len(monthlySoc['3'][:])), monthlySoc['3'][:], label = "Mar",color='red')
-    ax3.set_title('Mar')
+            ax3.set_ylabel('SOC')
+            ax3.plot(range(len(self.price)), self.price, label = "price")
+            ax3.plot(range(len(self.monthlySoc['3'][:])), self.monthlySoc['3'][:], label = "Mar",color='red')
+            ax3.set_title('Mar')
 
-    ax4.set_ylabel('SOC')
-    ax4.plot(range(len(monthlySoc['4'][:])), monthlySoc['4'][:], label = "Apr",color='red')
-    ax4.plot(range(len(price)), price, label = "price")
-    ax4.set_title('Apr')
+            ax4.set_ylabel('SOC')
+            ax4.plot(range(len(self.monthlySoc['4'][:])), self.monthlySoc['4'][:], label = "Apr",color='red')
+            ax4.plot(range(len(self.price)), self.price, label = "price")
+            ax4.set_title('Apr')
 
-    ax5.set_ylabel('SOC')
-    ax5.plot(range(len(monthlySoc['5'][:])), monthlySoc['5'][:], label = "May",color='red')
-    ax5.plot(range(len(price)), price, label = "price")
-    ax5.set_title('May')
+            ax5.set_ylabel('SOC')
+            ax5.plot(range(len(self.monthlySoc['5'][:])), self.monthlySoc['5'][:], label = "May",color='red')
+            ax5.plot(range(len(self.price)), self.price, label = "price")
+            ax5.set_title('May')
 
-    ax6.set_ylabel('SOC')
-    ax6.plot(range(len(monthlySoc['6'][:])), monthlySoc['6'][:], label = "Jun",color='red')
-    ax6.plot(range(len(price)), price, label = "price")
-    ax6.set_title('Jun')
+            ax6.set_ylabel('SOC')
+            ax6.plot(range(len(self.monthlySoc['6'][:])), self.monthlySoc['6'][:], label = "Jun",color='red')
+            ax6.plot(range(len(self.price)), self.price, label = "price")
+            ax6.set_title('Jun')
 
-    ax7.set_ylabel('SOC')
-    ax7.plot(range(len(monthlySoc['7'][:])), monthlySoc['7'][:], label = "July",color='red')
-    ax7.plot(range(len(price)), price, label = "price")
-    ax7.set_title('July')
+            ax7.set_ylabel('SOC')
+            ax7.plot(range(len(self.monthlySoc['7'][:])), self.monthlySoc['7'][:], label = "July",color='red')
+            ax7.plot(range(len(self.price)), self.price, label = "price")
+            ax7.set_title('July')
 
-    ax8.set_ylabel('SOC')
-    ax8.plot(range(len(monthlySoc['8'][:])), monthlySoc['8'][:], label = "Aug",color='red')
-    ax8.plot(range(len(price)), price, label = "price")
-    ax8.set_title('Aug')
+            ax8.set_ylabel('SOC')
+            ax8.plot(range(len(self.monthlySoc['8'][:])), self.monthlySoc['8'][:], label = "Aug",color='red')
+            ax8.plot(range(len(self.price)), self.price, label = "price")
+            ax8.set_title('Aug')
 
-    ax9.set_ylabel('SOC')
-    ax9.plot(range(len(monthlySoc['9'][:])), monthlySoc['9'][:], label = "Sep",color='red')
-    ax9.plot(range(len(price)), price, label = "price")
-    ax9.set_title('Sep')
+            ax9.set_ylabel('SOC')
+            ax9.plot(range(len(self.monthlySoc['9'][:])), self.monthlySoc['9'][:], label = "Sep",color='red')
+            ax9.plot(range(len(self.price)), self.price, label = "price")
+            ax9.set_title('Sep')
 
-    ax10.set_ylabel('SOC')
-    ax10.plot(range(len(monthlySoc['10'][:])), monthlySoc['10'][:], label = "Oct",color='red')
-    ax10.plot(range(len(price)), price, label = "price")
-    ax10.set_title('Oct')
+            ax10.set_ylabel('SOC')
+            ax10.plot(range(len(self.monthlySoc['10'][:])), self.monthlySoc['10'][:], label = "Oct",color='red')
+            ax10.plot(range(len(self.price)), self.price, label = "price")
+            ax10.set_title('Oct')
 
-    ax11.set_ylabel('SOC')
-    ax11.plot(range(len(monthlySoc['11'][:])), monthlySoc['11'][:], label = "Nov",color='red')
-    ax11.plot(range(len(price)), price, label = "price")
-    ax11.set_title('Nov')
+            ax11.set_ylabel('SOC')
+            ax11.plot(range(len(self.monthlySoc['11'][:])), self.monthlySoc['11'][:], label = "Nov",color='red')
+            ax11.plot(range(len(self.price)), self.price, label = "price")
+            ax11.set_title('Nov')
 
-    ax12.set_ylabel('SOC')
-    ax12.plot(range(len(monthlySoc['12'][:])), monthlySoc['12'][:], label = "Dec",color='red')
-    ax12.plot(range(len(price)), price, label = "price")
-    ax12.set_title('Dec')
+            ax12.set_ylabel('SOC')
+            ax12.plot(range(len(self.monthlySoc['12'][:])), self.monthlySoc['12'][:], label = "Dec",color='red')
+            ax12.plot(range(len(self.price)), self.price, label = "price")
+            ax12.set_title('Dec')
 
-    sub1.set_ylabel('Power')
-    sub1.plot(range(len(monthlyRemain['1'][:])), monthlyRemain['1'][:],color='green')  
-    sub1.plot(range(len(monthlyRemain['1'][:])), np.zeros(95),'--')
+            sub1.set_ylabel('Power')
+            sub1.plot(range(len(self.monthlyRemain['1'][:])), self.monthlyRemain['1'][:],color='green')  
 
-    sub2.set_ylabel('Power')
-    sub2.plot(range(len(monthlyRemain['2'][:])), monthlyRemain['2'][:],color='green')  
+            sub2.set_ylabel('Power')
+            sub2.plot(range(len(self.monthlyRemain['2'][:])), self.monthlyRemain['2'][:],color='green')  
 
-    sub3.set_ylabel('Power')
-    sub3.plot(range(len(monthlyRemain['3'][:])), monthlyRemain['3'][:],color='green')  
+            sub3.set_ylabel('Power')
+            sub3.plot(range(len(self.monthlyRemain['3'][:])), self.monthlyRemain['3'][:],color='green')  
 
-    sub4.set_ylabel('Power')
-    sub4.plot(range(len(monthlyRemain['4'][:])), monthlyRemain['4'][:],color='green')  
+            sub4.set_ylabel('Power')
+            sub4.plot(range(len(self.monthlyRemain['4'][:])), self.monthlyRemain['4'][:],color='green')  
 
-    sub5.set_ylabel('Power')
-    sub5.plot(range(len(monthlyRemain['5'][:])), monthlyRemain['5'][:],color='green')  
+            sub5.set_ylabel('Power')
+            sub5.plot(range(len(self.monthlyRemain['5'][:])), self.monthlyRemain['5'][:],color='green')  
 
-    sub6.set_ylabel('Power')
-    sub6.plot(range(len(monthlyRemain['6'][:])), monthlyRemain['6'][:],color='green')  
+            sub6.set_ylabel('Power')
+            sub6.plot(range(len(self.monthlyRemain['6'][:])), self.monthlyRemain['6'][:],color='green')  
 
-    sub7.set_ylabel('Power')
-    sub7.plot(range(len(monthlyRemain['7'][:])), monthlyRemain['7'][:],color='green')  
+            sub7.set_ylabel('Power')
+            sub7.plot(range(len(self.monthlyRemain['7'][:])), self.monthlyRemain['7'][:],color='green')  
 
-    sub8.set_ylabel('Power')
-    sub8.plot(range(len(monthlyRemain['8'][:])), monthlyRemain['8'][:],color='green')  
+            sub8.set_ylabel('Power')
+            sub8.plot(range(len(self.monthlyRemain['8'][:])), self.monthlyRemain['8'][:],color='green')  
 
-    sub9.set_ylabel('Power')
-    sub9.plot(range(len(monthlyRemain['9'][:])), monthlyRemain['9'][:],color='green')  
+            sub9.set_ylabel('Power')
+            sub9.plot(range(len(self.monthlyRemain['9'][:])), self.monthlyRemain['9'][:],color='green')  
 
-    sub10.set_ylabel('Power')
-    sub10.plot(range(len(monthlyRemain['10'][:])), monthlyRemain['10'][:],color='green')  
+            sub10.set_ylabel('Power')
+            sub10.plot(range(len(self.monthlyRemain['10'][:])), self.monthlyRemain['10'][:],color='green')  
 
-    sub11.set_ylabel('Power')
-    sub11.plot(range(len(monthlyRemain['11'][:])), monthlyRemain['11'][:],color='green')  
+            sub11.set_ylabel('Power')
+            sub11.plot(range(len(self.monthlyRemain['11'][:])), self.monthlyRemain['11'][:],color='green')  
 
-    sub12.set_ylabel('Power')
-    sub12.plot(range(len(monthlyRemain['12'][:])), monthlyRemain['12'][:],color='green')  
+            sub12.set_ylabel('Power')
+            sub12.plot(range(len(self.monthlyRemain['12'][:])), self.monthlyRemain['12'][:],color='green')  
 
-    fig.tight_layout()
-    fig.savefig('pic/plot.png')
+        elif self.mode == 'load':
+            pass
 
-    network = agent.get_architecture()
-    print(network)
-    # Close agent and environment
-    agent.close()
-    environment.close()
+        fig.tight_layout()
+        fig.savefig('pic/plot.png')
+
+
+    def showNnConfig(self):
+        network = self.agent.get_architecture()
+        print(network)
+
+    def __del__(self):
+        # Close agent and environment
+        self.agent.close()
+        self.environment.close()
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
